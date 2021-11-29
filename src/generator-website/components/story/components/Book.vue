cad099f6b4544819de3e071e762f846514bd3c7f
AssafAharon <assafaharon7@gmail.com>
2021-11-29 21:28:56 +0200

    Made page Vue component. Works. Need to make Book and all this process more generic.

--- src/generator-website/components/story/components/Book.vue
+++ src/generator-website/components/story/components/Book.vue
@@ -1,53 +1,21 @@
 <template>
   <div class="book" ref="bookDiv">
-    <div class="p1 paper" ref="p1Div">
-      <div class="front">
-        <div class="f1 front-content">
-          <h1>Front 1</h1>
-        </div>
-      </div>
-
-      <div class="back">
-        <div class="b1 back-content">
-          <h1>Back 1</h1>
-        </div>
-      </div>
-    </div>
-
-    <div class="p2 paper" ref="p2Div">
-      <div class="front">
-        <div class="f2 front-content">
-          <h1>Front 2</h1>
-        </div>
-      </div>
-
-      <div class="back">
-        <div class="b2 back-content">
-          <h1>Back 2</h1>
-        </div>
-      </div>
-    </div>
-
-    <div class="p3 paper" ref="p3Div">
-      <div class="front">
-        <div class="f3 front-content">
-          <h1>Front 3</h1>
-        </div>
-      </div>
-
-      <div class="back">
-        <div class="b3 back-content">
-          <h1>Back 3</h1>
-        </div>
-      </div>
-    </div>
+    <TwoSidedPageComponent :pageNumber="1" :pageZIndex="3" pageFrontSide="Front 1" pageBackSide="Back 1" ref="p1"></TwoSidedPageComponent>
+    <TwoSidedPageComponent :pageNumber="2" :pageZIndex="2" pageFrontSide="Front 2" pageBackSide="Back 2" ref="p2"></TwoSidedPageComponent>
+    <TwoSidedPageComponent :pageNumber="3" :pageZIndex="1" pageFrontSide="Front 3" pageBackSide="Back 3" ref="p3"></TwoSidedPageComponent>
   </div>
 </template>
 
 <script lang="ts">
 
-import { Emit, Ref, Vue } from "vue-property-decorator";
+import { Emit, Options, Ref, Vue } from "vue-property-decorator";
+import TwoSidedPageComponent from "./TwoSidedPage.vue";
 
+@Options({
+  components: {
+    TwoSidedPageComponent
+  }
+})
 export default class BookComponent extends Vue {
 
   currentLocation = 1;
@@ -56,9 +24,9 @@ export default class BookComponent extends Vue {
 
   @Ref() bookDiv!: HTMLDivElement;
 
-  @Ref() p1Div!: HTMLDivElement;
-  @Ref() p2Div!: HTMLDivElement;
-  @Ref() p3Div!: HTMLDivElement;
+  @Ref() p1!: TwoSidedPageComponent;
+  @Ref() p2!: TwoSidedPageComponent;
+  @Ref() p3!: TwoSidedPageComponent;
 
   @Emit()
   openBook(): boolean {
@@ -74,6 +42,7 @@ export default class BookComponent extends Vue {
     else {
       this.bookDiv.style.transform = "translateX(100%)";
     }
+
     return false;
   }
 
@@ -82,18 +51,15 @@ export default class BookComponent extends Vue {
       switch (this.currentLocation) {
         case 1: {
           this.openBook();
-          this.p1Div.classList.add("flipped");
-          this.p1Div.style.zIndex = "1";
+          this.p1.flipToNextPage();
           break;
         }
         case 2: {
-          this.p2Div.classList.add("flipped");
-          this.p2Div.style.zIndex = "2";
+          this.p2.flipToNextPage();
           break;
         }
         case 3: {
-          this.p3Div.classList.add("flipped");
-          this.p3Div.style.zIndex = "3";
+          this.p3.flipToNextPage();
           this.closeBook(false);
           break;
         }
@@ -111,19 +77,16 @@ export default class BookComponent extends Vue {
       switch (this.currentLocation) {
         case 2: {
           this.closeBook(true);
-          this.p1Div.classList.remove("flipped");
-          this.p1Div.style.zIndex = "3";
+          this.p1.flipToPrevPage();
           break;
         }
         case 3: {
-          this.p2Div.classList.remove("flipped");
-          this.p2Div.style.zIndex = "2";
+          this.p2.flipToPrevPage();
           break;
         }
         case 4: {
           this.openBook();
-          this.p3Div.classList.remove("flipped");
-          this.p3Div.style.zIndex = "1";
+          this.p3.flipToPrevPage();
           break;
         }
         default: {
@@ -139,11 +102,7 @@ export default class BookComponent extends Vue {
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+
 
 .book {
   position: relative;
@@ -152,65 +111,16 @@ export default class BookComponent extends Vue {
   transition: transform 0.5s;
 }
 
-.paper {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  perspective: 1500px;
-}
 
-.front,
-.back {
-  background-color: white;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  transform-origin: left;
-  transition: transform 0.5s;
-}
 
-.front {
-  z-index: 1;
-  backface-visibility: hidden;
-  border-left: 1px solid pink;
-}
 
-.back {
-  z-index: 0;
-}
 
-.front-content,
-.back-content {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
-.back-content {
-  transform: rotateY(180deg);
-}
 
-.p1 {
-  z-index: 3;
-}
 
-.p2 {
-  z-index: 2;
-}
 
-.p3 {
-  z-index: 1;
-}
 
-/* Pper flip effect*/
-.flipped .front,
-.flipped .back {
-  transform: rotateY(-180deg);
-}
+
+
+
 </style>
---
+++ src/generator-website/components/story/components/TwoSidedPage.vue
@@ -0,0 +1,101 @@
+<template>
+  <div :class="pageHTMLClass" :ref="pageHTMLRef" :style="pageStyle">
+    <div class="front">
+      <div class="front-content">
+        <h1>{{ pageFrontSide }}</h1>
+      </div>
+    </div>
+
+    <div class="back">
+      <div class="back-content">
+        <h1>{{ pageBackSide }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from "vue-property-decorator";
+
+export default class TwoSidedPageComponent extends Vue {
+  @Prop() pageNumber!: number;
+  @Prop() pageZIndex!: number;
+  @Prop() pageFrontSide!: string;
+  @Prop() pageBackSide!: string;
+  @Prop() pageZIndexForPrevFlip!: string;
+
+  get pageHTMLClass(): string {
+    return `p${this.pageNumber} paper`;
+  }
+
+  get pageHTMLRef(): string {
+    return `p${this.pageNumber}Div`;
+  }
+
+  get pageStyle(): string {
+    return `
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      perspective: 1500px;
+      z-index: ${this.pageZIndex}`;
+  }
+
+  flipToPrevPage(): void {
+    const pageDiv: HTMLDivElement = this.$refs[this.pageHTMLRef] as HTMLDivElement;
+    pageDiv.classList.remove("flipped");
+    pageDiv.style.zIndex = this.pageZIndex.toString();
+  }
+
+  flipToNextPage(): void {
+    const pageDiv: HTMLDivElement = this.$refs[this.pageHTMLRef] as HTMLDivElement;
+    pageDiv.classList.add("flipped");
+    pageDiv.style.zIndex = this.pageNumber.toString();
+  }
+}
+</script>
+
+<style scoped>
+.front,
+.back {
+  background-color: white;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  transform-origin: left;
+  transition: transform 0.5s;
+}
+
+.front {
+  z-index: 1;
+  backface-visibility: hidden;
+  border-left: 1px solid pink;
+}
+
+.back {
+  z-index: 0;
+}
+
+.front-content,
+.back-content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.back-content {
+  transform: rotateY(180deg);
+}
+
+/* Paper flip effect*/
+.flipped .front,
+.flipped .back {
+  transform: rotateY(-180deg);
+}
+</style>
